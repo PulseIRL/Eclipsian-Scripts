5cf94e0a2ef554824493463471bae6b5a178de94
PulseIRL <148807529+PulseIRL@users.noreply.github.com>
2023-10-23 22:06:45 +0100

    Delete Scripts directory

--- Scripts/custom-changes.mjs
+++
@@ -1,7 +0,0 @@
-Hooks.once("init", () => {
-  CONFIG.DND5E.damageTypes.shadow = "Shadow";
-  CONFIG.DND5E.damageResistanceTypes.shadow = "Shadow";
-  CONFIG.DND5E.spellSchools.sha = "Shadow Weaving"
-  CONFIG.DND5E.spellSchools.cut = "Cutting Arts"
-  CONFIG.DND5E.spellSchools.mec = "Mechanical"
-});
